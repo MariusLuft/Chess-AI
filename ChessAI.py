a9bf7ab28eb196bd7717fefad364db5ea7bd93f9
Marius Luft <luftmarius97@gmail.com>
2021-05-11 10:40:10 +0200

    minMax two moves ahead

--- ChessAI.py
+++ ChessAI.py
@@ -9,21 +9,31 @@ def findRandomMove(validMoves):
 
 def findBestMove(gameState, validMoves):
     turnMutiplyer = 1 if gameState.whiteToMove else -1
-    maxScore = -CHECKMATE
-    bestMove = None
+    opponentsMinMaxScore = CHECKMATE
+    bestPlayerMove = None
     for playerMove in validMoves:
+        # every legal move for the player
         gameState.makeMove(playerMove)        
-        if gameState.checkMate:
-            score = CHECKMATE
-        elif gameState.staleMate:
-            score = STALEMATE
-        else:
-            score = turnMutiplyer * getMaterialScore(gameState.board) # makes the score positive for easy comparison
-        if score > maxScore:
-            maxScore = score
-            bestMove = playerMove
+        opponentsMoves = gameState.getValidMoves()
+        opponentMaxScore = - CHECKMATE
+        for opponentsMove in opponentsMoves:
+            # finds best response from opponent
+            gameState.makeMove(opponentsMove)
+            if gameState.checkMate:
+                score = -turnMutiplyer *  CHECKMATE # inverted so it's the negative opponent score
+            elif gameState.staleMate:
+                score = STALEMATE
+            else:
+                score = -turnMutiplyer * getMaterialScore(gameState.board) # inverted so it's the negative opponent score
+            if score > opponentMaxScore:
+                opponentMaxScore = score 
+            gameState.undoMove()
+        # if it has the weakest response the move is best for us
+        if opponentMaxScore < opponentsMinMaxScore:
+            opponentsMinMaxScore = opponentMaxScore
+            bestPlayerMove = playerMove
         gameState.undoMove()
-    return bestMove
+    return bestPlayerMove
 
 def getMaterialScore(board):
     score = 0
